ef1e232de8b42509985065ada2d5881b122c9c10
Mr. E-Scholar <mister.escholar@gmail.com>
2023-08-25 10:59:31 -0400

    Update script.js
    Now includes an alert action for when the user sets the time to a specific/desired time.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const hourHand = document.getElementById('hourHand');
 const minuteHand = document.getElementById('minuteHand');
 let targetElement = null;
+let dragging = false;
 
 document.addEventListener('mousedown', (event) => {
     if (event.target === hourHand || event.target === minuteHand) {
         targetElement = event.target;
+        dragging = true;
     }
 });
 
 document.addEventListener('mousemove', (event) => {
-    if (targetElement) {
+    if (dragging && targetElement) {
         const clock = targetElement.closest('svg');
         const rect = clock.getBoundingClientRect();
         const centerX = rect.left + rect.width / 2;
@@ -20,6 +22,30 @@ document.addEventListener('mousemove', (event) => {
     }
 });
 
-document.addEventListener('mouseup', () => {
+function endDrag() {
+    dragging = false;
+    if (targetElement) {
+        const hour = getCurrentTimeFromRotation(hourHand);
+        const minute = getCurrentTimeFromRotation(minuteHand);
+        
+        if (hour === 3 && minute === 15) {
+            alert("The time is set to 3:15!");
+        }
+    }
     targetElement = null;
-});
+}
+
+window.addEventListener('mouseup', endDrag);
+
+function getCurrentTimeFromRotation(element) {
+    const transformString = element.getAttribute('transform');
+    const match = /rotate\(([^,]+),/.exec(transformString);
+    let rotation = parseFloat(match[1]) % 360;
+    if (rotation < 0) rotation += 360;
+    if (element === hourHand) {
+        return Math.round(rotation / 30) % 12;
+    } else if (element === minuteHand) {
+        return Math.round(rotation / 6) % 60;
+    }
+}
+
